0ee5db6effe0e598d6d6e5ebda872cc43d8b6b7c
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2023-05-24 10:07:51 +0300

    first commit

---
+++ icd-11/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ICD-11</title>
+</head>
+<body>
+
+<h1>This is ICD-11 Service running in 5555 port</h1>
+    
+</body>
+</html>
---
+++ preventa-main-backend/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preventa-main-backend</title>
+</head>
+<body>
+
+<h1>This is Preventa-main-backend Service running in 8888 port</h1>
+    
+</body>
+</html>
